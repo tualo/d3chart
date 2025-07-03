c3b062170a80b5e4421628174a6686f268e3421b
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-07-03 09:11:20 +0200

    fix js loader

--- src/Routes/JsLoader.php
+++ src/Routes/JsLoader.php
@@ -1,24 +1,41 @@
 <?php
+
 namespace Tualo\Office\D3Chart\Routes;
 
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
+use Tualo\Office\Basic\RouteSecurityHelper;
 use Tualo\Office\Basic\IRoute;
 
-class JsLoader implements IRoute{
-    public static function register(){
-        BasicRoute::add('/jsd3chart/(?P<file>[\w.\/\-]+).js',function($matches){
+class JsLoader implements IRoute
+{
+    public static function register()
+    {
+        BasicRoute::add('/jsd3chart/(?P<file>[\w.\/\-]+).js', function ($matches) {
+            RouteSecurityHelper::serveSecureStaticFile(
+                $matches['file'],
+                dirname(__DIR__, 1) . '/js/lazy',
+                ['js'],
+                ['application/javascript']
+            );
 
-            if (file_exists(  dirname(__DIR__,1).'/js/lazy/'.$matches['file'].'.js' ) ){
+            /*
+            if (file_exists(dirname(__DIR__, 1) . '/js/lazy/' . $matches['file'] . '.js')) {
                 App::contenttype('application/javascript');
-                App::etagFile( dirname(__DIR__,1).'/js/lazy/'.$matches['file'].'.js' , true);
+                App::etagFile(dirname(__DIR__, 1) . '/js/lazy/' . $matches['file'] . '.js', true);
                 BasicRoute::$finished = true;
                 http_response_code(200);
-            }
-            
-            
-        },['get'],false);
-        BasicRoute::add('/jsd3/(?P<file>[\w.\/\-]+).js',function($matches){
+            }*/
+        }, ['get'], false);
+        BasicRoute::add('/jsd3/(?P<file>[\w.\/\-]+).js', function ($matches) {
+
+            RouteSecurityHelper::serveSecureStaticFile(
+                $matches['file'],
+                dirname(__DIR__, 1) . '/js/lib',
+                ['js'],
+                ['application/javascript']
+            );
+            /*
 
             if (file_exists(  dirname(__DIR__,1).'/js/lib/'.$matches['file'].'.js' ) ){
                 App::contenttype('application/javascript');
@@ -26,9 +43,7 @@ class JsLoader implements IRoute{
                 BasicRoute::$finished = true;
                 http_response_code(200);
             }
-            
-            
-        },['get'],false);
-
+                */
+        }, ['get'], false);
     }
 }
